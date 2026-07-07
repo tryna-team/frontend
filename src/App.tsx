2baad8a680dde94ad75f36b41a3f359a64aa84e7
skadms118 <skadms118@gmail.com>
2026-07-08 03:34:41 +0900

    chore: App.tsx 테스트 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import './App.css';
-import QuickModal from '@/features/event/components/QuickModal';
 
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
-      <QuickModal
-        message="이 이벤트를 삭제하시겠습니까?"
-        onConfirm={() => alert('삭제 확인')}
-        onClose={() => alert('모달 닫힘')}
-      />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
