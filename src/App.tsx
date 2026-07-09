2702cd577800f509fb6318aca5bc31db84e9d4be
harrysjuns <harrysjuns@gachon.ac.kr>
2026-07-10 00:40:55 +0900

    feat: 플랫 패턴 적용 예시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
+import HeroButtonExample from './components/common/Buttons_hero/HeroButton.example';
 
 function App() {
-  return <></>;
+  return <HeroButtonExample />;
 }
 
 export default App;
