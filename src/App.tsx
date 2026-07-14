10f5901bd2a67d0975f9edd8aa914dc9bcd38de5
skadms118 <skadms118@gmail.com>
2026-07-14 17:18:29 +0900

    import되는거 일단 빼놈..

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import WeekStrip from '@/features/calendar/components/WeekStrip';
-import ScheduleBanner from '@/components/common/ScheduleBanner/ScheduleBanner';
 import './App.css';
 
 function App() {
