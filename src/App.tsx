45411fc52b04b4be7825f8bd5a723840f9e520cc
UGangg <mororo_morori@naver.com>
2026-07-15 22:26:44 +0900

    feat: 웹 화면 비율 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+	  // 공통 테스트 화면 크기
+    // width : 390 ~ 402px
+    // height: 최소 840px, 콘텐츠에 따라 자동 증가
+    <div className="mx-auto min-h-[840px] w-full min-w-[390px] max-w-[402px] bg-background-white">
+      
+    </div>
+  );
 }
 
 export default App;
