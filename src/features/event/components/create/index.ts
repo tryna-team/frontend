15c1be874e36f57a29004f409b894c706c8a7364
UGangg <mororo_morori@naver.com>
2026-07-18 16:50:49 +0900

    feat: index에 불필요한 내용 제거

--- src/features/event/components/create/index.ts
+++ src/features/event/components/create/index.ts
@@ -1,12 +1,2 @@
-// src/features/event/components/create/index.ts
-
 export { default as DateRangeCalendar } from './DateRangeCalendar';
-
-export type {
-  CreateEventForm,
-  DateRange,
-  DateSelectionMode,
-  RepeatType,
-  TimePeriod,
-  TimeValue,
-} from './CreateEvent.types';
+export type { EventDateRange } from './CreateEvent.types';
